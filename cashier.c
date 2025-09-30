1af591e9be48586cc5e64612715b3acf955a915a
PacoG&G <ingfnoyola@gmail.com>
2025-09-30 11:17:27 -0600

    Fix: Restauramos cashier.c

--- cashier.c
+++ cashier.c
@@ -2,109 +2,97 @@
 
 int main(void) { 
     
-    int n = 0;
-    while(n != -1) {
+    int o = 0;
+    float bal = 0;
+    float aux = 0;
 
-        printf("%s", "Input a decimal number: \n");
+    float op1=-9999, op2=-9999, op3=-9999, op4=-9999;
+    char o1='N', o2='N', o3='N', o4='N', auxO='N';
 
-        if(scanf("%d", &n)) {
+    while(o != 4) {
 
-            if(n==-1) {
-                 printf("Closing program...\n");
-            } else {
-            
-                //CONVERSION A BINARIO
-                int aux = n;
-                int div = 1;
+        printf("\nSelect an option: \n");
+        printf("1-View balance\n2-Deposit\n3-Withdraw\n4-Close program\n\n");
 
-                //DIVISOR INICIA EN 1 Y SE INCREMENTA EN 2 (BITS) HASTA SER EL NUMERO MAS GRANDE POSIBLE
-                //PERO MENOR O IGUAL A N
-                while(div <= n) {
-                    div *= 2;
-                }
-                div /= 2;
-
-                printf("Decimal: %d\n", n);
-
-                printf("Binario: ");
-
-                while(div > 0) {
-                    printf("%d", aux/div);
-                    aux %= div;
-                    div /= 2;
-                }
-                printf("%s", "\n");
-
-                //CONVERSION A OCTAL
-                aux = n;
-                div = 1;
-
-                //DIVISOR INICIA EN 1 Y SE INCREMENTA POR 8 (OCTALES) HASTA SER EL NUMERO MAS GRANDE POSIBLE
-                //PERO MENOR O IGUAL A N
-                
-                printf("Octal: ");
-
-                while(div <= n) {
-                    div *= 8;
-                }
-                div /= 8;
-
-                while(div > 0) {
-                    printf("%d", aux/div);
-                    aux %= div;
-                    div /= 8;
-                }
-                printf("%s", "\n");
-
-                printf("Hexadecimal: ");
-
-                //CONVERSION A HEXADECIMAL
-                aux = n;
-                div = 1;
-
-                while(div <= aux) {
-                    div *= 16;
-                }
-                div /= 16;
-
-                int aux2 = 0;
-                while(div > 0) {
-                    aux2 = aux/div;
-                    switch(aux2) {
-                        case 10:
-                            printf("A");
-                            break;
-                        case 11:
-                            printf("B");
-                            break;
-                        case 12:
-                            printf("C");
-                            break;
-                        case 13:
-                            printf("D");
-                            break;
-                        case 14:
-                            printf("E");
-                            break;
-                        case 15:
-                            printf("F");
-                            break;
-                        default:
-                            printf("%d", aux2);
+        if(scanf("%d", &o)) {
+            switch(o) {
+                case 1:
+                    printf("\nCurrent balance: $%.2f\n", bal);
+                    printf("Last operations:\n");
+                    if(o1 != 'N') {
+                        printf("%s%.2f\n",(o1=='D')?"Deposit ":"Withdrawal: ", op1);
+                    } else{
+                        printf("No operations yet...\n");
                     }
-                    aux %= div;
-                    div /= 16;
+                    if(o2 != 'N') {
+                        printf("%s%.2f\n", (o2=='D')?"Deposit ":"Withdrawal: ", op2);
+                    }
+                    if(o3 != 'N') {
+                        printf("%s%.2f\n", (o3=='D')?"Deposit ":"Withdrawal: ", op3);
+                    }
+                    if(o4 != 'N') {
+                        printf("%s%.2f\n", (o4=='D')?"Deposit ":"Withdrawal: ", op4);
+                    }
+                    break;
+                case 2:
+                    printf("\nInput the amount to deposit:\n");
+                    scanf("%f", &aux);
 
-                }
-                printf("\n");
+                    if(aux<0) {
+                        printf("\nInput a valid amount (positive)!\n");
+                    }
+                    else {
+                        bal += aux;
+                        printf("New balance: $%.2f\n\n", bal);
+                        op4=op3;
+                        op3=op2;
+                        op2=op1;
+                        op1=aux;
+                        o4=o3;
+                        o3=o2;
+                        o2=o1;
+                        o1='D';
+                    }
+                    break;
+                case 3:
+                    printf("\nInput the amount to withdraw:\n");
+                    scanf("%f", &aux);
 
+                    if(aux<0) {
+                        printf("\nInput a valid amount (positive)!\n\n");
+                    }
+                    else {
+                        if(aux>bal) {
+                            printf("\nInsufficient funds!\n\n");
+                        }
+                        else {
+                            bal -= aux;
+                            printf("New balance: $%.2f\n\n", bal);
+                            op4=op3;
+                            op3=op2;
+                            op2=op1;
+                            op1=aux;
+                            o4=o3;
+                            o3=o2;
+                            o2=o1;
+                            o1='W';
+                        }
+                    }
+                    break;
+                case 4:
+                    printf("Closing program...\n\n");
+                    o=4;
+                    break;
+                default:
+                    printf("Input a valid option!\n\n");
+                    break;
             }
-
         }
         else {
-            printf("%s", "Input a decimal number!\n");
-            while(getchar() != '\n');
+            printf("Input a valid option!\n");
+            while( getchar() != '\n' );
         }
+
         
     }
     
