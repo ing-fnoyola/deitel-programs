7cf2a4c19401214ed78942d62c61735ab269da1c
PacoG&G <ingfnoyola@gmail.com>
2025-10-17 14:58:17 -0600

    First version

---
+++ Chapter5/guessNumber.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int guessNumber(int number, int guess);
+int validate(void);
+
+int main(void) {
+    srand(time(NULL)); //randomize rand() every iteration
+    int r = 0;
+
+    while( r != -1) {
+
+        int number = (rand() % 1000) + 1;
+        int guess = 0;
+        int win = 0;
+
+        printf("Im thinking in a number between 1-1000. Try to guess!\n");
+        int lock = scanf("%d", &guess);
+
+        if(lock == 1) {
+
+            if(guess == -1) {
+                r = -1;
+            } else {
+                win = guessNumber(number, guess);
+
+                while(win != 1) {
+                    int lock1 = scanf("%d", &guess);
+                    if(lock1 == 1) {
+                        win = guessNumber(number, guess);
+                    } else {    
+                        r = validate();
+                    }
+                }
+            }
+            
+
+        } else {
+            r = validate();
+        }
+      
+    }
+    printf("Closing program...\n");
+}
+
+int guessNumber(int number, int guess) {
+
+    if(number == guess) {
+        printf("Congrats! Are you using magic?\n");
+        return 1;
+    } else {
+        if(guess < number) {
+            printf("Too low buddy! Try again...\n");
+            return 0;
+        } else {
+            printf("Too high buddy! Try again...\n");
+            return 0;
+        }
+
+    }
+
+}
+
+int validate(void) {
+    if( feof(stdin) ) { //EOF - End of File 
+        printf("\n End of Input Detected\n");
+        return -1;
+    } else {
+        printf("Please input a valid number!\n\n");
+        while( getchar() != '\n' );
+        return 1;
+    }
+}
